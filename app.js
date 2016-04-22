ef3b26a552436bd6ef0a6aeafb3947102ce94bcc
Tanya <tanyalsweeney@yahoo.com>
2016-04-22 02:03:58 +0000

    Assignment 2 finally done

--- app.js
+++ app.js
@@ -1,25 +1,54 @@
 var express = require('express');
-var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(bodyParser.json()); //  support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true}));  //  support encoded bodies
+app.use(express.static(__dirname + "/public"));
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+var data = [
+        {buzzword: "iot", plain: "Random stuff that's connected to the web"},
+        {buzzword: "dom", plain: "Code that makes up a web page"},
+        {buzzword: "woff", plain: "Free fonts"},
+        {buzzword: "geolocation", plain: "Where are you now?"},
+        {buzzword: "api", plain: "Get data from Facebook or Twitter"},
+        {buzzword: "minified", plain: "Unreadable code. Don't do it."},
+        {buzzword: "unicode", plain: "readable"},
+        {buzzword: "ux", plain: "Is it usable?"},
+        {buzzword: "canvas", plain: "Draw stuff on the interwebs"}
+    ];
 
 app.get('/', function(req,res){
-    res.render('home');
+    res.type("text/html");
+    res.sendfile("./public/home.html");
 });
 
-app.post('/api/users', function(req,res){
-    var user_id = req.body.id;
-    var token = req.body.token;
-    var geo = req.body.geo;
-    
-    res.send(user_id + ' ' + token + ' ' + geo);
+app.get('/about', function(req,res){
+    res.type("text/html");
+    res.sendfile("./public/about.html");
+});
+
+app.post('/search', function(req,res){
+    res.type("text/html");
+    var user_input = req.body.search_term;
+    var success = data.find(function(item) {
+        return item.buzzword == user_input;
+    });
+    if(success == undefined) {
+        var fail_message = "Sorry. " + user_input + " not found."
+        res.send(fail_message);
+    } else {
+        var success_message = "Success! " + user_input
+            + " means " + success.plain + "!";
+        res.send(success_message);
+    }
+});
+
+app.use(function(req, res, next){
+    res.status(404);
+    res.type("text/html");
+    res.sendfile("./public/404.html");
 });
 
 app.set('port', process.env.PORT || 3000);
