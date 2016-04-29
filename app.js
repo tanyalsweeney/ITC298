5dcd69cd406af0c5ef111b95a68dba30d18a2cd3
Tanya <tanyalsweeney@yahoo.com>
2016-04-29 03:39:30 +0000

    not pretty but all buttons work

--- app.js
+++ app.js
@@ -34,6 +34,50 @@ app.post('/search', function(req,res){
     }
 });
 
+app.post('/update', function(req,res){
+    res.type("text/html");
+    var user_term = req.body.search_term;
+    var user_definition = req.body.definition;
+    var success = jargon.updateJargon(user_term, user_definition);
+    if(success == undefined || user_definition.length < 2) {
+        var fail_message = "fail";
+        res.send(fail_message);
+    } else {
+        var success_message = '<div style="font: 16px arial; color: #999; padding: 50px;">Success!<br><span style="font: bold 22px arial">' + success.buzzword
+             + "</span><br>is now defined as " + success.plain + "</div>!";
+        res.send(success_message);
+    }
+});
+
+app.post('/add', function(req,res){
+    res.type("text/html");
+    var user_term = req.body.search_term;
+    var user_definition = req.body.definition;
+    var term_exists = jargon.getJargon(user_term);
+    var success = jargon.addJargon(user_term,user_definition);
+    if(success == undefined || term_exists != undefined) {
+        var fail_message = "fail";
+        res.send(fail_message);
+    } else {
+        var success_message = "term added";
+        res.send(success_message);
+    }
+});
+
+app.post('/remove', function(req,res){
+    res.type("text/html");
+    var user_term = req.body.search_term;
+    var term_exists = jargon.getJargon(user_term);
+    var success = jargon.removeJargon(user_term);
+    if(success == undefined || term_exists == undefined) {
+        var fail_message = 'fail';
+        res.send(fail_message);
+    } else {
+        var success_message = "term deleted";
+        res.send(success_message);
+    }
+});
+
 
 app.use(function(req, res, next){
     res.status(404);
